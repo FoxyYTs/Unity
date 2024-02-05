5e6d1d1e91790b22abb71e3fc0eaf6a69b9b2e6f
FoxyYTs <foxy200442@gmail.com>
2024-02-05 10:02:52 -0500

    asd

---
+++ My project (1)/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour{
+
+    public float Speed;
+    public float jumpForce;
+    public int saltoMax;
+    public LayerMask Ground;
+
+    private bool ViewRight = true;
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private int saltoRestante;
+
+    private Animator anime;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        anime = GetComponent<Animator>();
+        saltoRestante = saltoMax;
+    }
+
+    // Update is called once per frame
+    void Update(){  
+        movimiento();
+        salto();
+    }
+
+    bool suelo(){
+        RaycastHit2D hit = Physics2D.BoxCast(bc.bounds.center, new Vector2(bc.bounds.size.x, bc.bounds.size.y), 0f, Vector2.down, .1f, Ground);
+        return hit.collider != null;
+    }
+
+    void salto(){
+        if(suelo()){
+            saltoRestante = saltoMax;
+            anime.SetBool("saltando", false);
+        }else{
+            anime.SetBool("saltando", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && saltoRestante > 0){
+            
+            saltoRestante--;
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    void movimiento(){
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        if(inputMovimiento != 0){
+            anime.SetBool("corriendo", true);
+        }else{
+            anime.SetBool("corriendo", false);
+        }
+        direccion(inputMovimiento);
+        rb.velocity = new Vector2(inputMovimiento * Speed, rb.velocity.y);
+
+        
+    }
+
+    void direccion(float inputMovimiento){
+        if ((ViewRight && inputMovimiento < 0) || (!ViewRight && inputMovimiento > 0)){
+            ViewRight = !ViewRight;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+
+}
---
+++ My project (1)/Assets/Scripts/GameManajer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManajer : MonoBehaviour{
+
+    public int puntosTotales { get { return puntosTotales; } }
+    private int puntosTotales;
+    
+    public void SumarPuntos()
+}
